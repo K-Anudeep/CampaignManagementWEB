a89ce4a7345f46c78fc3c390fb6b844699994f9c
SaiAnudeepK <anudeeph8@gmail.com>
2021-06-21 14:29:40 +0530

    Add project files.

---
+++ MarketingManagement.API/DataContext/MarketingMgmtDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MarketingManagement.API.DataContext
+{
+    public class MarketingMgmtDBContext : DbContext
+    {
+        public MarketingMgmtDBContext(DbContextOptions<MarketingMgmtDBContext> options) : base(options)
+        {
+        }
+    }
+}
