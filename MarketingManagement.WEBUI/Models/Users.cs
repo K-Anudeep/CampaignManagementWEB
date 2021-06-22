09df323bf0c697803268cc869b422853e8222ef7
SaiAnudeepK <anudeeph8@gmail.com>
2021-06-22 10:48:09 +0530

    Login WIP

---
+++ MarketingManagement.WEBUI/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketingManagement.WEBUI.Models
+{
+    public class Users
+    {
+        [Required]
+        public string UserId { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
