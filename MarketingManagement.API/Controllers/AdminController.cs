2bb7570955f4147ce2f5e293dfdf7cd40174d565
SaiAnudeepK <anudeeph8@gmail.com>
2021-06-22 15:02:18 +0530

    User and Admin Controller Update

---
+++ MarketingManagement.API/Controllers/AdminController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MarketingManagement.API.DataContext;
+using MarketingManagement.API.Models.Entities;
+using MarketingManagement.API.Models.Validations;
+using MarketingManagement.API.Services;
+
+namespace MarketingManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly MarketingMgmtDBContext _context;
+        private readonly AdminServices _admin;
+        public AdminController(MarketingMgmtDBContext context)
+        {
+            _context = context;
+            _admin = new AdminServices(context);
+        }
+
+        //POST: api/Admin/Add
+        [Route("Add")]
+        [HttpPost]
+        public ActionResult<Users> AddUsers([Bind("FullName, LoginID, Password, DateOfJoin, Address, IsAdmin")] Users user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (UsersExists(user.LoginID))
+                    {
+                        throw new Exception("Login ID already exists!");
+                    }
+
+                    //Send User details to Repo for insertion to DB
+                    _admin.AddUser(user);
+                    return Ok();
+                }
+                else
+                {
+                    throw new Exception("Model State is not valid");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        //Discontinue User
+        // PUT: api/Admin/Update/{id}
+        [Route("DiscontinueUser/{userId}")]
+        [HttpPut]
+        public ActionResult<Users> DiscontinueUser(int userId)
+        {
+            try
+            {
+                //Check if Discontinued or not
+                var isDiscontinued = _admin.DiscontinueUser(userId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        // GET: api/Admin/GetAll
+        [Route("GetAll")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Users>> GetUsers()
+        {
+            return _admin.DisplayUsers().ToList();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("GetOneUser/{userId}")]
+        public ActionResult<Users> GetUsers(int userId)
+        {
+            //Calls User repo to get a specific user by Id
+            var users = _admin.OneUser(userId);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+
+        private bool UsersExists(string loginId)
+        {
+            return _context.Users.Any(e => e.LoginID == loginId);
+        }
+    }
+}
---
+++ MarketingManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MarketingManagement.API.DataContext;
+using MarketingManagement.API.Models.Entities;
+using MarketingManagement.API.Models.Validations;
+using MarketingManagement.API.Services;
+
+namespace MarketingManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AccessCheck _accessCheck;
+        public UsersController(MarketingMgmtDBContext context)
+        {
+            _accessCheck = new AccessCheck(context);
+        }
+
+        //POST: api/Users/Login
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult UserValidation(string loginId, string password)
+        {
+            // Validate a User with their given Credentials
+            var validation = _accessCheck.Validation(loginId, password);
+            if(validation != null)
+            {
+                //Check for Admin or Executive
+                Users Check = _accessCheck.AdminCheck(loginId);
+                if(Check.IsAdmin == 1)
+                {
+                    return Content("Admin");
+                }
+                else
+                {
+                    return Content("Executive");
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
