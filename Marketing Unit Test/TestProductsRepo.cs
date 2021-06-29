46a688aefcbce6fd1ce59dfc64b1c2bee45a742e
PavanKalyanE <pavanchiyaan71@gmail.com>
2021-06-29 14:43:00 +0530

    Unit Testing Done for products and sales

---
+++ Marketing Unit Test/TestProductsRepo.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using MarketingManagement.API.Models.Repositories.Interfaces;
+using MarketingManagement.API.Models.Entities;
+using System.Collections.Generic;
+using Moq;
+using System.Linq;
+namespace MarketingManagement.API
+{
+    public class TestProductsRepo
+    {
+        private readonly IProductsRepo _repo;
+        public TestProductsRepo()
+        {
+            IList<Products> products = new List<Products>() {
+            new Products(){ProductID=100,ProductName="XYZ-Idea Mouse",Description="This is a mid-range gaming mouse capable of high precision combined with low-latency",UnitPrice=4999},
+            new Products(){ProductID=116,ProductName="XYZ Keyboard",Description="keyboard for gaming",UnitPrice=5678},
+            };
+            var mockRepo = new Mock<IProductsRepo>();
+
+            mockRepo.Setup(repo => repo.DisplayProducts()).Returns(products.ToList());
+            ///mockRepo.Setup(repo => repo.GetDonator(It.IsAny<int>())).Returns((int i) => products.SingleOrDefault(x => x.Pid == i));
+            mockRepo.Setup(repo => repo.AddProducts(It.IsAny<Products>())).Callback((Products item) =>
+            {
+                item = new Products() { ProductID = 118, ProductName = "XYZ CPU", Description = "CPU", UnitPrice = 20000 };
+                products.Add(item);
+            }).Verifiable();
+            mockRepo.SetupAllProperties();
+            _repo = mockRepo.Object;
+        }
+        [Fact]
+        public void TestAddProducts()
+        {
+            var productsdetails = new Products() { ProductID = 118, ProductName = "XYZ CPU", Description = "CPU", UnitPrice = 20000 };
+            _repo.AddProducts (productsdetails);
+
+            Assert.Equal(1, 1);
+        }
+        [Fact]
+        public void DisplayProducts()
+        {
+            int expected = 2;
+            var productsdetails = _repo.DisplayProducts();
+            Assert.Equal(expected, productsdetails.Count);
+        }
+    }
+}
---
+++ Marketing Unit Test/TestSalesRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using MarketingManagement.API.Models.Repositories.Interfaces;
+using MarketingManagement.API.Models.Entities;
+using System.Collections.Generic;
+using Moq;
+using System.Linq;
+
+namespace MarketingManagement.API
+{
+    public class TestSalesRepo
+    {
+        private readonly ISalesRepo _repo;
+        public TestSalesRepo()
+        {
+            IList<Sales> sales = new List<Sales>()
+            {
+                new Sales() { OrderID = 100,LeadID= 100 ,ShippingAddress="Hyderabad",BillingAddress="Hyderabad",CreatedON = Convert.ToDateTime("23-06-2021"), PaymentMode = "CashonDelivery"},
+                new Sales() { OrderID = 109,LeadID= 104 ,ShippingAddress="Vizag",BillingAddress="Vizag",CreatedON = Convert.ToDateTime("03-04-2021"), PaymentMode = "Debit Card" }
+            };
+            var mockRepo = new Mock<ISalesRepo>();
+
+            mockRepo.Setup(repo => repo.ViewSales()).Returns(sales.ToList());
+            mockRepo.SetupAllProperties();
+            _repo = mockRepo.Object;
+        }
+        [Fact]
+        public void TestViewSales()
+        {
+            int expected = 2;
+            var salesdetails = _repo.ViewSales();
+            Assert.Equal(expected, salesdetails.Count);
+        }
+    }
+}
